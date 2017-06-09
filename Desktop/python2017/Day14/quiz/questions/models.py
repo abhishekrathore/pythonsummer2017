3373d77fa1ebc86adb3516671078628479fc488b
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-06-09 17:29:30 +0530

    models shell

--- Desktop/python2017/Day14/quiz/questions/models.py
+++ Desktop/python2017/Day14/quiz/questions/models.py
@@ -12,13 +12,13 @@ class Person(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    roll_no = models.IntegerField(default=123,primary_key=True)
+    roll_no = models.IntegerField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,null=True)
     joining_date = models.DateField(null=True)
     graduated = models.NullBooleanField(null=True)
-    year = models.CharField(choices=years,default="first",max_length=30)
-    class_room_id = models.ForeignKey('ClassRoom',on_delete=models.CASCADE,default=1)
+    year = models.CharField(choices=years,max_length=30)
+    class_room = models.ForeignKey('ClassRoom',on_delete=models.CASCADE)
 
 
     def __str__(self):
@@ -26,8 +26,7 @@ class Person(models.Model):
 
 class ClassRoom(models.Model):
     name = models.CharField(max_length=30)
-    time = models.TimeField()
-    class_id = models.AutoField(primary_key=True)
+    time = models.TimeField(null=True)
 
     def __str__(self):
         return self.name
